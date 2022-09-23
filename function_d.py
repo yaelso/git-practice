9d9d4445762fa4d07ddbd1a2691863c38738ff20
Melley Gebretatios <gmelley4@gmail.com>
2022-09-23 17:29:00 -0400

    Melleys new function

--- function_d.py
+++ function_d.py
@@ -2,7 +2,14 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+
+    max = numbers[0]
+    for num in numbers:
+        if num > max:
+            max = num
+
+    return max
+#return max(numbers)
 
 
 if __name__ == "__main__":
